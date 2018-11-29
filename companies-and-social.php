d133d41eab3c6df674660993f9aea77c54983415
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-11-29 14:57:49 +0000

    Added companies and social media page and stylesheet

---
+++ companies-and-social.php
@@ -0,0 +1,13 @@
+<!--
+
+  EVENT SPONSORING COMPANIES AND SOCIAL MEDIA COMPONENT
+
+ -->
+
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/companies-and-social.css">
+</head>
+<body>
+
+</body>
--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <link rel="stylesheet" type="text/css" href="css/site.css"> <!-- includes the preset site font sizes, font styles and colours -->
   </head>
   <body>
-    
+
   </body>
 </html>
