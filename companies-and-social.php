33ed04ff4b26a61943d8630aeeacc0bc0d2332d8
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-11-29 15:33:16 +0000

    Added event sponsoring companies

--- companies-and-social.php
+++ companies-and-social.php
@@ -9,5 +9,24 @@
   <link rel="stylesheet" type="text/css" href="css/companies-and-social.css">
 </head>
 <body>
+  <div class="container companies-and-social-wrapper">
+    <!-- Event sponsoring companies -->
+    <div class="container sponsoring-companies-wrapper">
+      <!-- List for the sponsoring companies -->
+      <ul>
+        <li><img src="images/companies/capital-management.png" alt="Capital Management Logo"/></li>
+        <li><img src="images/companies/vfestival.png" alt="V Festival"/></li>
+        <li><img src="images/companies/festival-republic.png" alt="Festival Republic"/></li>
+        <li><img src="images/companies/summer-camp.png" alt="Summer Camp"/></li>
+      </ul>
+    </div>
+    <!-- Facebook feed -->
+    <div class="container facebook-wrapper">
+      
+    </div>
+    <!-- Twitter feed -->
+    <div class="container twitter-wrapper">
 
+    </div>
+  </div>
 </body>
