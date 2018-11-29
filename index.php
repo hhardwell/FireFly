fee0a63b2db8228024acae9253aa5cfdefb5f28d
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-11-29 14:48:21 +0000

    Added default index.php layout

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/site.css"> <!-- includes the preset site font sizes, font styles and colours -->
+  </head>
+  <body>
+    
+  </body>
+</html>
