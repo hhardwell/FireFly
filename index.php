7b3b16af67ad32417a0f4604e278908a2672a102
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-11-29 15:02:22 +0000

    Added CDNs for Bootstrap and Jquery

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <html>
   <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!-- Latest compiled and minified CSS for bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <!-- jQuery library -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><!-- Latest compiled JavaScript for bootstrap -->
+
     <link rel="stylesheet" type="text/css" href="css/site.css"> <!-- includes the preset site font sizes, font styles and colours -->
   </head>
   <body>
