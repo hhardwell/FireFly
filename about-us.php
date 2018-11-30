27c8b42a179bd4346bf0b4c6528bc6611d7abed9
AlexHirlam <42181344+AlexHirlam@users.noreply.github.com>
2018-11-30 16:31:51 +0000

    Continued to work on the about us page. Adding pictures of memebers of staff.

---
+++ about-us.php
@@ -0,0 +1,80 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="css/about-us.css">
+</head>
+<body>
+
+
+<div class="container main-container">
+  <div class="intro-container">
+    <h1>About Us</h1>
+    <p> Firefly is an event management company based out of Norwich. Our experienced and well adapted team preform
+      a number of services for a wide range of clients. </br>
+      Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.
+    </p>
+    <img src="images/TextLogo-Transparent.png">
+  </div>
+
+<hr>
+
+  <div class="history-container">
+    <h2> History of FireFly Events </h2>
+      <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker. </p>
+  </div>
+</div>
+
+<div class="container main-container">
+  <h2>FireFly Members</h2>
+
+<div class="staff-image-container">
+  <div class="staff">
+    <img class="img-fluid" src="images/staff-images/kayleigh-hall.jpg">
+    <p>Name and Role </p>
+  </div>
+
+
+  <div class="staff">
+    <img class="img-fluid" src="images/staff-images/matt-hir.jpg">
+    <p>Name and Role </p>
+  </div>
+</div>
+
+<div class="staff-image-container">
+  <div class="staff">
+    <img class="img-fluid" src="images/staff-images/alex-chenery-howes.jpg">
+    <p>Name and Role </p>
+  </div>
+
+
+  <div class="staff">
+    <img class="img-fluid"src="images/staff-images/kayleigh-halls.jpg">
+    <p>Name and Role </p>
+  </div>
+</div>
+
+<div class="staff-image-container">
+  <div class="staff">
+    <img class="img-fluid"src="images/staff-images/andrew-hall.jpg">
+    <p>Name and Role </p>
+  </div>
+
+  <div class="staff">
+    <img class="img-fluid"src="images/staff-images/andrew-halls.jpg">
+    <p>Name and Role </p>
+  </div>
+</div>
+
+<div class="staff-image-container">
+  <div class="staff">
+    <img class="img-fluid"src="images/staff-images/kayleigh-hall-swag.jpg">
+    <p>Name and Role </p>
+  </div>
+
+  <div class="staff">
+    <img class="img-fluid"src="images/staff-images/kayleigh-hall-posh.jpg">
+    <p>Name and Role </p>
+  </div>
+</div>
+
+</div>
+
+</body>
