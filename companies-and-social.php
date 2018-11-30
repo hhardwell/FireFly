e929889e552d38de19fac496a553ae1ef6fc78cd
Manvidas Barvicius <manvidas.barvicius@netmatters.local>
2018-11-30 08:51:32 +0000

    Added Facebook Feed

--- companies-and-social.php
+++ companies-and-social.php
@@ -22,7 +22,27 @@
     </div>
     <!-- Facebook feed -->
     <div class="container facebook-wrapper">
-      
+      <div class="card">
+        <img class="card-img-top" src="images/facebook/facebook-background.png" alt="Card image cap">
+        <div class="card-body">
+          <img src="images/logo.jpg" alt="Fire Fly Events"/>
+          <h5 class="card-title description-font description-button-font-size">FireFly Events</h5> <!-- TODO: Update the font size -->
+          <h5 class="card-title description-font footer-navigation-font-size">1064 Likes</h5> <!-- TODO:
+                                                                                                Update like counter
+                                                                                                Update the font size -->
+          <div class="card-button">
+            <div class="card-button-wrapper">
+              <div class="btn description-font"><i class="fab fa-facebook"></i> Like Page</div>
+              <div class="btn description-font">Contact Us</div>
+            </div>
+          </div>
+        </div>
+        <div class="card-body">
+          <img src="images/logo.jpg" alt="Fire Fly Events"/>
+          <h5 class="card-title description-font description-button-font-size">FireFly Events</h5>
+          <p class="card-text description-font">All done for another year! Time to put all the pyro, lasers, led, staging, trakway, toilets, fencing and much more back in it's box! #creamfields2018 #eventmanagement #aplevent</p>
+        </div>
+      </div>
     </div>
     <!-- Twitter feed -->
     <div class="container twitter-wrapper">
