46032a3b737a124a5807bc479a7fd68cd0884d0f
AlexHirlam <42181344+AlexHirlam@users.noreply.github.com>
2018-11-30 11:36:36 +0000

    Added Header

---
+++ header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+
+<body>
+
+<div class=nav-background>
+  <div class="header container description-font">
+    <div class="logo">
+      <a href="#">
+        <img src="images/full-title_orange-white.jpg">
+      </a>
+    </div>
+
+    <button class="navbar-toggler hidden-lg" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="top-nav collapse navbar-collapse" id="navbarNav">
+
+      <ul class="nav navbar-nav navbar-expand-lg navbar-light bg-ligh pull-xs-right">
+
+        <li class="nav-item">
+            <button type="button" class="btn" href="#">
+              Home
+            </button>
+        </li>
+
+        <li class="nav-item">
+            <button type="button" class="btn" href="#">
+              What's on
+            </button>
+        </li>
+
+        <li class="nav-item">
+            <button type="button" class="btn" href="#">
+              About us
+            </button>
+        </li>
+
+        <li class="nav-item">
+            <button type="button" class="btn" href="#">
+              Our Events
+            </button>
+        </li>
+
+        <li class="nav-item">
+            <button type="button" class="btn" href="#">
+              Contact us
+            </button>
+        </li>
+
+    </ul>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <div class="logo">
+    <img src="images/logo.jpg">
+  </div>
+
+  <div class="top-nav">
+  <div class="bottom-nav">
+  </div>
+</div>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ <a class="navbar-brand" href="#">Navbar</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="navbarNav">
+   <ul class="navbar-nav">
+     <li class="nav-item active">
+       <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Features</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">Pricing</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link disabled" href="#">Disabled</a>
+     </li>
+   </ul>
+ </div>
+</nav>
+
+</body>
