5ba56f66111e6b7e61918419519a2c173d897197
AlexHirlam <42181344+AlexHirlam@users.noreply.github.com>
2018-11-30 14:57:47 +0000

    Finshed the header and footer, needs a couple of minor responsive tweaks

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/footer.css">
+</head>
+
+<body>
+
+<div class="footer-background title-font">
+  <nav class=" container navbar navbar-light">
+    <div class="logo-footer">
+      <a href="#">
+        <img src="images/logo.jpg">
+      </a>
+    </div>
+
+      <p class="copyright-info">This is some text where all the copyright information will be placed when we can think of something to put here.</p>
+
+    <div class="footer-nav">
+      <a class="footer-nav-item nav-link" href="#">Home </span></a>
+      <p class="seperator">|</p>
+      <a class="footer-nav-item nav-link" href="#">What's on</a>
+      <p class="seperator">|</p>
+      <a class="footer-nav-item nav-link" href="#">About us</a>
+      <p class="seperator">|</p>
+      <a class="footer-nav-item nav-link" href="#">Our Events</a>
+      <p class="seperator">|</p>
+      <a class="footer-nav-item nav-link" href="#">Contact us</a>
+    </div>
+
+
+    </div>
+  </div>
+
+
+</body>
