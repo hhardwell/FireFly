c4cbb18d1216f978529ec6d850416a8ee768bddb
AlexHirlam <42181344+AlexHirlam@users.noreply.github.com>
2018-12-03 16:57:47 +0000

    Started the home page testimonial containers.

---
+++ testimonials.php
@@ -0,0 +1,37 @@
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/testimonials.css">
+
+</head>
+
+<div class="container">
+  <div class="main-container testi-container">
+    <div class="testi-logo ">
+      <img class= "img-fluid" src="images/companies/festival-republic.png">
+    </div>
+
+      <p>"FireFly Events did a great job setting up and helping us one of our first venues. We will definitely be using them again."</p>
+
+  </div>
+
+  <div class="main-container testi-container">
+
+      <p>"FireFly Events did a great job setting up and helping us one of our first venues. We will definitely be using them again."</p>
+
+    <div class="testi-logo ">
+      <img class= "img-fluid" src="images/companies/capital-management.png">
+    </div>
+  </div>
+
+  <div class="main-container testi-container">
+    <div class="testi-logo ">
+      <img class= "img-fluid" src="images/companies/summer-camp.png">
+    </div>
+
+      <p>"FireFly Events did a great job setting up and helping us one of our first venues. We will definitely be using them again."</p>
+
+  </div>
+
+  <button class="read-more testi-button"> Read More... </button>
+
+</div>
