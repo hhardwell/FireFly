859a5f6c833550b09025dfd07f07b28e5facb4c8
samhop87 <44196563+samhop87@users.noreply.github.com>
2018-12-17 15:38:04 +0000

    Began writing up the whats-on page and an extra stylesheet.

---
+++ whats-on.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="css/site.css">
+  <link rel="stylesheet" type="text/css" href="css/whats-on.css">
+
+</head>
+
+<body>
+<!-- THE NAVBAR GOES HERE -->
+
+
+<!-- Page title -->
+
+<div class="content-container">
+<div class="page-title header-font">
+  <a href="#" class="card-font-size color-grey">What's</a><a href="#" class="card-font-size color-orange">On</a>
+</div>
+</div>
+
+
+<!-- Beginning of content -->
+
+<div class="content-container">
+<div class="description-font background-grey color-grey description-button-font-size descriptor">
+  <p>This is the "What's On" page and features the many exciting events that are coming up. I'm sure you're as thrilled as we are to learn about the FireFly events schedule and you'll obviously be planning your entire year around what we've got on. Or something.</p><p class="color-orange">Insert excitment here.</p>
+</div>
+</div>
+
+<div class="content-container">
+<div class="events">
+<div class="first testimonial-font-size">
+  <span>EVENT ONE</span>
+</div>
+
+<div class="second testimonial-font-size">
+  <span>EVENT ONE</span>
+</div>
+
+<div class="third testimonial-font-size">
+  <span>EVENT ONE</span>
+</div>
+</div>
+</div>
+
+
+
+
+</body>
