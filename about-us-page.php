ecc813f941ec92dba23f727bbecbb88719cf68bf
Jason Parfitt <jason.parfitt@netmatters.local>
2019-01-16 10:49:06 +0000

    chnaged fonts on about-us-page to be raleway, and fixed staff images overlapping

--- about-us-page.php
+++ about-us-page.php
@@ -6,68 +6,62 @@
 
 <head>
   <link rel="stylesheet" type="text/css" href="css/about-us.css">
+  <link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
 <body>
+  <div class="container container-flex description-font">
+    <div class="inner-about-container">
+      <div class="container main-container body-container">
+        <?php include "about-us-data.php" ?>
+        <img class=img-fluid src="images/TextLogo-Transparent.png">
 
-<div class=" container container-flex">
-  <div class="container">
-    <div class="container main-container">
-      <?php include "about-us-data.php" ?>
-      <img class=img-fluid src="images/TextLogo-Transparent.png">
-
-
-      <hr>
+        <hr>
 
         <div class="history-container">
-          <h2> History of FireFly Events </h2>
-            <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker. </p>
+          <h2 class="header-font"> History of FireFly Events </h2>
+            <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,
+              quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu
+              não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60,
+              quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração
+              eletrônica como Aldus PageMaker. </p>
         </div>
+      </div>
+
+      <div class="container main-container staff-container">
+        <h2 class="header-font">FireFly Members</h2>
+
+        <div class="staff-image-container">
+          <div class="staff">
+            <img class="img-fluid" src="images/staff-images/kayleigh-hall.jpg">
+            <p>Kayleigh Hall</p>
+          </div>
+
+          <div class="staff">
+            <img class="img-fluid" src="images/staff-images/matt-hir.jpg">
+            <p>Name and Role </p>
+          </div>
+
+          <div class="staff">
+            <img class="img-fluid" src="images/staff-images/alex-chenery-howes.jpg">
+            <p>Name and Role </p>
+          </div>
+
+          <div class="staff">
+            <img class="img-fluid"src="images/staff-images/kayleigh-halls.jpg">
+            <p>Name and Role </p>
+          </div>
+
+          <div class="staff">
+            <img class="img-fluid"src="images/staff-images/andrew-hall.jpg">
+            <p>Name and Role </p>
+          </div>
+
+          <div class="staff">
+            <img class="img-fluid"src="images/staff-images/andrew-halls.jpg">
+            <p>Name and Role </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-
-  <div class="container main-container staff-container">
-    <h2>FireFly Members</h2>
-
-  <div class="staff-image-container">
-    <div class="staff">
-      <img class="img-fluid" src="images/staff-images/kayleigh-hall.jpg">
-      <p>Kayleigh Hall</p>
-    </div>
-
-
-    <div class="staff">
-      <img class="img-fluid" src="images/staff-images/matt-hir.jpg">
-      <p>Name and Role </p>
-    </div>
-  </div>
-
-<div class="staff-image-container">
-    <div class="staff">
-      <img class="img-fluid" src="images/staff-images/alex-chenery-howes.jpg">
-      <p>Name and Role </p>
-    </div>
-
-
-    <div class="staff">
-      <img class="img-fluid"src="images/staff-images/kayleigh-halls.jpg">
-      <p>Name and Role </p>
-    </div>
-  </div>
-
-<div class="staff-image-container">
-    <div class="staff">
-      <img class="img-fluid"src="images/staff-images/andrew-hall.jpg">
-      <p>Name and Role </p>
-    </div>
-
-    <div class="staff">
-      <img class="img-fluid"src="images/staff-images/andrew-halls.jpg">
-      <p>Name and Role </p>
-    </div>
-</div>
-
-
-  </div>
-  </div>
-</div>
 </body>
