511a2b4d97b301a34b982f03f6dc4ef2481305de
jason.parfitt <jasparfitt@gmail.com>
2019-03-08 08:58:21 +0000

    added login.html

--- footer.php
+++ footer.php
@@ -5,40 +5,35 @@
   UPDATED BY: Manvidas (15/01/2019) -> Adding links to the footer
  -->
 
-<!DOCTYPE html>
+
 
 <head>
   <link rel="stylesheet" type="text/css" href="css/footer.css">
 </head>
 
 <footer>
+  <div class="footer-background title-font">
+    <nav class="footer-container container navbar navbar-light">
+      <div class="logo-footer">
+        <a href="#">
+          <img src="images/logo.jpg">
+        </a>
+      </div>
 
-<div class="footer-background title-font">
-  <nav class="footer-container container navbar navbar-light">
-    <div class="logo-footer">
-      <a href="#">
-        <img src="images/logo.jpg">
-      </a>
-    </div>
+        <p class="copyright-info">This is some text where all the copyright information will be placed when we can think of something to put here.</p>
 
-      <p class="copyright-info">This is some text where all the copyright information will be placed when we can think of something to put here.</p>
-
-    <div class="footer-nav">
-      <a class="footer-nav-item nav-link" href="/FireFly/">Home </span></a>
-      <p class="seperator">|</p>
-      <a class="footer-nav-item nav-link" href="/FireFly/whats-on.php">What's On</a>
-      <p class="seperator">|</p>
-      <a class="footer-nav-item nav-link" href="/FireFly/about-us-page.php">About Us</a>
-      <p class="seperator">|</p>
-      <a class="footer-nav-item nav-link" href="/FireFly/our-events.php">Our Events</a>
-      <p class="seperator">|</p>
-      <a class="footer-nav-item nav-link" href="/FireFly/contact-us.php">Contact Us</a>
-    </div>
-
-
-    </div>
+      <div class="footer-nav">
+        <a class="footer-nav-item nav-link" href="/FireFly/">Home</a>
+        <p class="seperator">|</p>
+        <a class="footer-nav-item nav-link" href="/FireFly/whats-on.php">What's On</a>
+        <p class="seperator">|</p>
+        <a class="footer-nav-item nav-link" href="/FireFly/about-us-page.php">About Us</a>
+        <p class="seperator">|</p>
+        <a class="footer-nav-item nav-link" href="/FireFly/our-events.php">Our Events</a>
+        <p class="seperator">|</p>
+        <a class="footer-nav-item nav-link" href="/FireFly/contact-us.php">Contact Us</a>
+      </div>
+    </nav>
   </div>
-
-
 </footer>
 </body>
